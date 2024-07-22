9a7fc8bd72205a31ff8018239bcd94dcae350d37
paracus2 <rodrigo-dru@hotmail.com>
2024-07-22 20:27:34 -0300

    Agregar archivos de proyecto.

---
+++ SistemaVenta.Model/Clidocumentos.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SistemaVenta.Model;
+
+public partial class Clidocumentos
+{
+    public int IdNumeroDocumento { get; set; }
+
+    public int UltimoNumero { get; set; }
+
+    public DateTime? FechaRegistro { get; set; }
+}
